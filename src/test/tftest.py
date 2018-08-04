0809a3d0d2e782a15ddac8cb127eda5c1bc86cb9
zwp <1258271806@qq.com>
2018-08-04 09:13:14 +0800

    init

---
+++ src/test/tftest.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2018年8月4日
+
+@author: zwp12
+'''
+
+import tensorflow as tf;
+
+def run():
+    
+    pass;
+
+
+if __name__ == '__main__':
+    run();
+    pass
